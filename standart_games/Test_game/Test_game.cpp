884a3f7b2bb95277042d1a30f547eb175d61eda7
Peytob <38009435+Peytob@users.noreply.github.com>
2018-05-06 23:04:11 +0300

    Add files via upload

--- standart_games/Test_game/Test_game.cpp
+++ standart_games/Test_game/Test_game.cpp
@@ -22,24 +22,19 @@ void Test_game::initialization_textures()
 
 void Test_game::initialization_elements()
 {
-	element_list.emplace_back("NONE", "", 1, NONE);
-	element_list.emplace_back("AIR", "", 2, AIR);
-	element_list.emplace_back("WATER", "", 3, WATER);
-	element_list.emplace_back("FIRE", "", 4, FIRE);
-	element_list.emplace_back("EARTH", "", 5, EARTH);
-	element_list.emplace_back("PLANT", "", 6, PLANT);
-	element_list.emplace_back("ANIMAL", "", 7, ANIMAL);
-	element_list.emplace_back("MAGIC", "", 8, MAGIC);
-	element_list.emplace_back("CIVILIZATION", "", 9, CIVILIZATION);
-	element_list.emplace_back("HUMAN", "", 10, HUMAN);
-	element_list.emplace_back("FOOD", "", 11, FOOD);
-	element_list.emplace_back("DARK", "", 12, DARK);
-	element_list.emplace_back("OTHER", "", 13, OTHER);
-	element_list.emplace_back("DARK_BLUE", "", 14, DARK_BLUE);
-	element_list.emplace_back("CRIMSON", "", 15, CRIMSON);
-	element_list.emplace_back("ORANGE", "", 16, ORANGE);
-	element_list.emplace_back("JUNGLE", "", 17, JUNGLE);
-	element_list.emplace_back("EVIL", "", 18, EVIL);
+	/* Demonstration of colors */
+	element_list.emplace_back("AIR_COLOR", "", 1, AIR_COLOR);
+	element_list.emplace_back("FIRE_COLOR", "", 2, FIRE_COLOR);
+	element_list.emplace_back("EARTH_COLOR", "", 3, EARTH_COLOR);
+	element_list.emplace_back("WATER_COLOR", "", 4, WATER_COLOR);
+	element_list.emplace_back("LIGHT_GREEN_COLOR", "", 5, LIGHT_GREEN_COLOR);
+	element_list.emplace_back("PURPLE_COLOR", "", 6, PURPLE_COLOR);
+	element_list.emplace_back("GREY_COLOR", "", 7, GREY_COLOR);
+	element_list.emplace_back("LIGHT_PINK_COLOR", "", 8, LIGHT_PINK_COLOR);
+	element_list.emplace_back("DARK_COLOR", "", 9, DARK_COLOR);
+	element_list.emplace_back("DARK_RED_COLOR", "", 10, DARK_RED_COLOR);
+	element_list.emplace_back("DARK_BLUE_COLOR", "", 11, DARK_BLUE_COLOR);
+	element_list.emplace_back("DARK_GREEN_COLOR", "", 12, DARK_GREEN_COLOR);
 }
 
 void Test_game::initialization_reactions()
@@ -51,6 +46,6 @@ void Test_game::initialization_started_elements()
 {
 	for (int i = 0; i < element_list.size(); ++i)
 	{
-		started_elements.push_back(i);
+		started_elements.push_back(element_list[i].get_id());
 	}
 }
